f9c42b15f4fb3ab50f560e6bc09b84d6bf519277
pjourdan <pauljourdanbaez@gmail.com>
2018-03-23 09:31:55 -0300

    Implementacion de modelos

--- PrintAgentRF/Program.cs
+++ PrintAgentRF/Program.cs
@@ -8,12 +8,19 @@ namespace PrintAgentRF
 {
     static class Program
     {
+        /*
+        Utilizacion del log4net en el sistema
+        */
+        private static readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         /// <summary>
         /// Punto de entrada principal para la aplicación.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            logger.Debug("Iniciando Sistema PARestFul");
+            logger.Info("Iniciando Sistema PARestFul");
+            logger.Warn("Alerta de inicio de sistema");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new PrintAgenPrincipal());
--- PrintAgentRF/Properties/AssemblyInfo.cs
+++ PrintAgentRF/Properties/AssemblyInfo.cs
@@ -34,3 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+//Linea donde especifica donde el log4net debe buscar su configuracion
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
